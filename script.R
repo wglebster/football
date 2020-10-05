aa295202fd2200741379391b8fbe2c8bd44d5538
wglebster <wglebster@gmail.com>
2020-10-05 13:27:01 +0100

    update raw data

---
+++ script.R
@@ -0,0 +1,6 @@
+library(tidyverse)
+
+premier_league_18_19 <- read_csv("data/england-premier-league-matches-2018-to-2019-stats.csv", skip_empty_rows = TRUE)
+View(premier_league_18_19)
+premier_league_18_19_1 <- read_csv("data/england-premier-league-matches-2018-to-2019-stats (1).csv", skip_empty_rows = TRUE)
+View(premier_league_18_19_1)
